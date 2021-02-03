3fa1d0c75dcdd749134c9b075b345b43722454e2
mujahdn18 <78481702+mujahdn18@users.noreply.github.com>
2021-02-03 20:41:32 +0800

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "config/config.php";
-    if (isset($_SESSION['user'])) {
-        echo "<script>window.location='".base_url('dashboard')."';</script>";
-    } else {
-        echo "<script>window.location='".base_url('auth/login.php')."';</script>";
-    }
-?>
